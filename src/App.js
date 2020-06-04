cd44c332fbe6928f86fc43f238a48e87d833305d
Bruno Silva <brunosilvinosilva@gmail.com>
2020-06-04 16:59:33 +0100

    added useReducer hook and created reducer to increment, decrement and reset count

--- src/App.js
+++ src/App.js
@@ -1,18 +1,39 @@
-import React, {useContext} from 'react';
+import React, {useContext, useReducer} from 'react';
 import { userContext }from "./index"
 
+const initialState = {
+  count: 0
+}
+
+function reducer(state, action) {
+  switch (action.type) {
+    
+    case "increment":
+      return {
+        count: state.count + 1
+      }
+    case "decrement":
+      return {
+        count: state.count - 1
+      }
+      case "reset":
+        return initialState
+
+    default:
+      return state
+  }
+}
 
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialState)
 
   const value = useContext(userContext)
   return (
-    // <div>
-    //   <userContext.Consumer>
-    //     {value => <div>Hello {value}</div>}
-    //   </userContext.Consumer>
-    // </div>
     <div>
-      Hello {value}
+      Count: {state.count}<br/>
+      <button onClick={() => dispatch({type: "increment"})}>Increment</button><br/>
+      <button onClick={() => dispatch({type: "decrement"})}>Decrement</button><br/>
+      <button onClick={() => dispatch({type: "reset"})}>Reset</button>
     </div>
   );
 }
