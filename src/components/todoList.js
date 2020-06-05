74fca01259dec4bc1b2576f46a26272373c3084c
Bruno Silva <brunosilvinosilva@gmail.com>
2020-06-05 13:42:43 +0100

    styled todoList component while also adding two dispatches to delete and toggle a todo

--- src/components/todoList.js
+++ src/components/todoList.js
@@ -3,13 +3,29 @@ import TodosContext from "../context"
 
 export default function Todolist() {
     const {state, dispatch } = useContext(TodosContext)
+    const title = state.todos.length > 0 ? `${state.todos.length} Todos` : "Nothing to do"
     
     return (
-        <div>
-            <ul>
+        <div className="container mx-auto max-w-md text-center font-mono">
+            <h1 className="text-bold">{title}</h1>
+            <ul className="list-reset text-black p-0">
                 {state.todos.map(todo => {
-                    return <li key={todo.id}>
-                        <span>{todo.text}</span>
+                    return <li key={todo.id} 
+                    className="flex items-center bg-orange-dark 
+                    border-black border-dashed
+                    border-2 my-2 py-4">
+                        <span
+                        onDoubleClick={() => dispatch({type: "TOGGLE_TODO", payload: todo})}
+                        className={`cursor-pointer flex-1 ml-12 ${todo.complete && "line-through text-grey-darkest"}`}
+                        >{todo.text}</span>
+                        <button>
+                            <img src="https://icon.now.sh/edit/0050c5" alt="Edit Icon"
+                            className="h-6"/>
+                        </button>
+                        <button onClick={() => dispatch({type: "DELETE_TODO", payload: todo})}>
+                            <img src="https://icon.now.sh/delete/8b0000" alt="Delete Icon"
+                            className="h-6"/>
+                        </button>
                     </li>
                 })}
             </ul>
--- src/reducer.js
+++ src/reducer.js
@@ -1,6 +1,24 @@
 export default function Reducer(state, action) {
     switch (action.type) {
 
+        case "TOGGLE_TODO":
+           const toggle_todos = state.todos.map(t => {
+               return t.id === action.payload.id ? 
+                {...action.payload, complete: !action.payload.complete}
+                 : t }
+            )
+            return {
+                ...state,
+                todos: toggle_todos
+            }
+
+        case "DELETE_TODO":
+            const filter = state.todos.filter(t => t.id !== action.payload.id)
+            return {
+                ...state,
+                todos: filter
+            }
+
         default:
             return state
     }
