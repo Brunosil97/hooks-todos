85f0f37751603e55fddb794c789e4ccea5116f14
Bruno Silva <brunosilvinosilva@gmail.com>
2020-06-14 16:56:42 +0100

    code cleanup

--- src/reducer.js
+++ src/reducer.js
@@ -1,41 +1,17 @@
-// import uuidv4 from "uuidv4"
-const { uuid } = require('uuidv4');
-
 export default function Reducer(state, action) {
     switch (action.type) {
 
         case "GET_TODOS":
-            return {
-                ...state,
-                todos: action.payload
-            }
+            return { ...state, todos: action.payload };
     
         case "ADD_TODO":
-            // if(!action.payload) {
-            //     return state 
-            // }
-            // if (state.todos.findIndex(t => t.text === action.payload) > -1) {
-            //     return state 
-            // }
             const added_todo = [...state.todos, action.payload];
-            return {
-                ...state,
-                todos: added_todo
-            }
+            return { ...state, todos: added_todo }
 
         case "SET_CURRENT_TODO":
-            return {
-                ...state,
-                currentTodo: action.payload
-            }
+            return { ...state, currentTodo: action.payload };
 
         case "UPDATE_TODO":
-            // if(!action.payload) {
-            //     return state
-            // }
-            // if (state.todos.findIndex(t => t.text === action.payload) > -1) {
-            //     return state 
-            // }
             const updatedTodo = {...action.payload}
             const updatedIndex = state.todos.findIndex(t => t.id === state.currentTodo.id)
             const updatedTodos = [
@@ -44,30 +20,20 @@ export default function Reducer(state, action) {
                 ...state.todos.slice(updatedIndex + 1)
             ]
 
-            return {
-                ...state,
-                currentTodo: {},
-                todos: updatedTodos
-            }
+            return { ...state, currentTodo: {}, todos: updatedTodos };
         
         case "TOGGLE_TODO":
            const toggle_todos = state.todos.map(t => 
                t.id === action.payload.id ? 
                 action.payload : t 
             )
-            return {
-                ...state,
-                todos: toggle_todos
-            }
+            return { ...state, todos: toggle_todos };
 
         case "DELETE_TODO":
             const filter = state.todos.filter(t => t.id !== action.payload.id)
             const isRemovedTodo = state.currentTodo.id === action.payload.id ? {} : state.currentTodo
-            return {
-                ...state,
-                currentTodo: isRemovedTodo,
-                todos: filter
-            };
+
+            return { ...state, currentTodo: isRemovedTodo, todos: filter };
 
         default:
             return state
